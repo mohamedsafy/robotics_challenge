3dd95d1845b244bf8f4374ff6acab083a0aa5a27
mohamedsafy <mohammadsafyyt@gmail.com>
2023-04-12 12:25:47 -0700

    new file:   fake_camera.py

---
+++ robotics_challenge/nodes/fake_camera.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+def main():
+	rospy.init_node("fake_camera")
+	pub = rospy.Publisher("fake_camera", String, queue_size=100)
+	
+	rospy.sleep(0.5)
+	pub.publish("Baby don't hurt me, don't hurt me, no more!")
+	
+if __name__ == "__main__":
+	main()
