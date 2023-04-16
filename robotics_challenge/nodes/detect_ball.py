f4a35295d19efae23ffe2405055015744f0182ea
mohamedsafy <mohammadsafyyt@gmail.com>
2023-04-16 08:24:42 -0700

    modified:   robotics_challenge/CMakeLists.txt 	modified:   robotics_challenge/launch/start.launch 	modified:   robotics_challenge/nodes/detect_ball.py 	modified:   robotics_challenge/nodes/moves.py

--- robotics_challenge/nodes/detect_ball.py
+++ robotics_challenge/nodes/detect_ball.py
@@ -8,6 +8,10 @@
 #this is absolute stupidity, if python had variables pointers we wouldn't need to do this bullcrap. Note: I fucking hate python (1)
 
 import rospy
+import cv2
+import numpy as np
+
+from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Pose, Point
 from sensor_msgs.msg import Image
 from robotics_challenge.msg import SnapshotBallsLocation
@@ -17,19 +21,93 @@ class BallsDetector:
 
 		#Variables
 		self.poses = []
+		self.bridge = CvBridge()
+		self.image_received = False
+		self.ball_is_taken=False
 
-	def does_frame_have_ball(self, stream):
+	def img_callback(self, stream):
 		#TODO: Implement a function that is capable of taking bunch of images and finding balls in them, once the function finds a ball, it will be appended to the poses list
 		
 		#print('Image recieved!!!!')
+		# Convert image to OpenCV format
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(stream, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+		self.image_received = True
+		self.image = cv_image
+		#self.show_image(cv_image)
+			
+		self.does_frame_have_ball(cv_image)
+
+
+	def does_frame_have_ball(self, img):
+
+
 		
+		hsv_frame  =  cv2.cvtColor(img,  cv2.COLOR_BGR2HSV)
+
+		hsv_frame  =  cv2.resize(hsv_frame,(640,300))
+		img  =  cv2.resize(img,(640,300))
 		
+		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+			    param1=50,param2=30,minRadius=0,maxRadius=0)
+			    
+		circles = np.uint16(np.around(circles))
 		
+		for i in circles[0,:]:
+			# draw the outer circle
+			cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+			# draw the center of the circle
+			cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+		'''
+		low_H=0
+		low_S=100
+		low_V=100
+		high_H=18
+		high_S=255
+		high_V=255
+
+
+		#low_H=25
+		#low_S=100
+		#low_V=100
+		#high_H=32
+		#high_S=255
+		#high_V=255
+
+
+		mask_frame=cv2.inRange(hsv_frame,  (low_H,  low_S,  low_V),  (high_H,  high_S,  high_V))
+		cv2.imshow("mask", mask_frame)
+		contours,  hierarchy  =  cv2.findContours(mask_frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		#_,  contours,  _=  cv2.findContours(mask_frame,  cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_SIMPLE)
+
+		X,Y,W,H=0,0,0,0
+		x,y,w,h=0,0,0,0
+
+		for  pic,  contour  in  enumerate(contours):
+			area =cv2.contourArea(contour)
+
+			if(area>30):
+
+				x,y,w,h=cv2.boundingRect(contour)
+			if(w*h>W*H):
+				X,Y,W,H= x,y,w,h
+
+		img  =  cv2.rectangle(img,  (X,  Y),(X  +  W,  Y  +  H),(0,  0,  255),  2)
+
+		cx  =  X+(W/2)
+		cy  =  Y+(W/2)
+		'''
+		cv2.imshow("window",  img)
+		cv2.waitKey(3)
 		
-		self.poses =[Pose(position = Point(x=1, y=1, z=0)),
-		Pose(position = Point(x=1, y=1, z=0)),
-		Pose(position = Point(x=1, y=1, z=0))]
-		
+	def show_image(self,img):
+		cv2.imshow("Image Window", img)
+		cv2.waitKey(3)
+				
 	def clear_balls(self):
 		self.poses = []
 
@@ -76,24 +154,23 @@ def watch_new_balls(detector, cManager):
 			cManager.send_snapshot()
 			cManager.balls_location = detector.poses #Refer to General remarks point 1
 			detector.poses =[]
-	#Initalize objects
-	detector = BallsDetector()
-	cManager = ConnectionManager(detector.poses, detector.does_frame_have_ball)
-
-	#Call startup_rountiune
 			
 	
 	rospy.sleep(1)
+	rospy.spin()
 def main():
 	#Initialize node
 	rospy.init_node('detect_ball_node')
 
 	#Initalize objects
 	detector = BallsDetector()
-	cManager = ConnectionManager(detector.poses, detector.does_frame_have_ball)
+	cManager = ConnectionManager(detector.poses, detector.img_callback)
+
+	rospy.spin()
+
 
 	#Call startup_rountiune
-	startup_routiune(detector, cManager)
+	#startup_routiune(detector, cManager)
 	
 	#Detect rest of balls after initial balls are detected
 	#watch_new_balls(detector, cManager)
--- robotics_challenge/nodes/moves.py
+++ robotics_challenge/nodes/moves.py
@@ -1,4 +1,80 @@
 #!/usr/bin/env python3
 
-#Implement movement manager: responsible for translating descisions to movements
-#interact with navigation and moveit packages
+import rospy
+import actionlib
+from robotics_challenge.msg import Ball
+from geometry_msgs.msg import Pose, Point
+from actionlib_msgs.msg import GoalStatus
+from move_base_msgs.msg import MoveBaseFeedback, MoveBaseGoal, MoveBaseAction
+#x=-1.1, y=0
+
+class MovementManager:
+
+	def __init__(self, cManager):
+	
+		self.cManager = cManager
+		
+	def move_to_target(self, ball):
+		# Create goal:
+		goal = MoveBaseGoal()
+
+		# Set random goal:
+		goal.target_pose.header.frame_id = "map"
+		goal.target_pose.header.stamp = rospy.Time.now()
+		goal.target_pose.pose.position.x = ball.position.position.x
+		goal.target_pose.pose.position.y = ball.position.position.y
+		goal.target_pose.pose.orientation.w = 1.0
+		
+		self.cManager.send_goal_move_base(goal)
+
+class ConnectionManager:
+	
+	def __init__(self):
+	
+		self.move_base = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+		self.move_base.wait_for_server(rospy.Duration(5.0))
+		rospy.logdebug("move_base is ready")
+		
+	def send_goal_move_base(self, goal):
+		
+		self.move_base.send_goal(goal, self.goal_status)
+		
+	
+	def goal_status(self, status, result):
+		""" Check the status of a goal - goal reached, aborted,
+		or rejected.
+		"""
+		
+		# Goal reached
+		if status == 3:
+			
+			rospy.loginfo("Goal succeeded")
+
+		# Goal aborted
+		if status == 4:
+			rospy.loginfo("Goal aborted")
+
+		# Goal rejected
+		if status == 5:
+			rospy.loginfo("Goal rejected")
+            
+def main():
+
+	rospy.init_node('movement_manager_node')
+	
+	cManager = ConnectionManager()
+	mManager = MovementManager(cManager)
+	
+
+	ball = Ball()
+	ball.position.position.x = -1.12
+	ball.position.position.y = 0
+	
+	mManager.move_to_target(ball)
+	
+
+if __name__=='__main__':
+	main()
+
+
+
