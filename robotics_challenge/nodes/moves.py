f8294b1b3124392ceffcb05a3f5e19b2af3d9fbe
Mohamed Safy <mohammadsafyyt@gmail.com>
2023-04-22 19:28:26 +0200

    new file:   robotics_challenge/launch/hough_circles.launch 	new file:   robotics_challenge/launch/move_base.launch 	new file:   robotics_challenge/launch/phase1_demo.launch 	renamed:    robotics_challenge/launch/start.launch -> robotics_challenge/launch/testing.launch 	new file:   robotics_challenge/launch/turtlebot3_manipulation_gazebo.launch 	modified:   robotics_challenge/nodes/moves.py 	modified:   robotics_challenge/nodes/test.py

--- robotics_challenge/nodes/moves.py
+++ robotics_challenge/nodes/moves.py
@@ -2,75 +2,150 @@
 
 import rospy
 import actionlib
+import math
+import moveit_commander
+import moveit_msgs.msg
 from robotics_challenge.msg import Ball
-from geometry_msgs.msg import Pose, Point
+from geometry_msgs.msg import Pose, Point, PolygonStamped, Point32
+from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
+from nav_msgs.msg import Odometry
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseFeedback, MoveBaseGoal, MoveBaseAction
-#x=-1.1, y=0
+
+
+OPEN = 0.9
+CLOSE = 0
+
+class ArmController:
+    
+    def __init__(self):
+        moveit_commander.roscpp_initialize([])
+        self.arm = moveit_commander.MoveGroupCommander("arm")
+        self.robot = moveit_commander.RobotCommander('robot_description')
+        self.gripper = self.robot.get_joint('gripper')
+
+    def set_pos(self, position):
+        self.arm.set_position_target(position)
+        self.arm.go(wait=True)
+    def set_pos_name(self, name):
+        self.arm.set_named_target(name)
+        self.arm.go(wait=True)
+    def open_gripper(self):
+        self.gripper.move(self.gripper.max_bound() * OPEN, True)
+
+    def close_gripper(self):
+        self.gripper.move(self.gripper.max_bound() * CLOSE, True)
+
+    def pick_ball(self, position):
+        position_above_ball = [position[0], position[1], position[2] + 0.1]
+        self.set_pos(position_above_ball)
+        self.open_gripper()
+        position_below_ball = [position[0] - 0.005, position[1], position[2]]
+        self.set_pos(position_below_ball)
+        #position_below_ball = [position[0] - 0.01, position[1], position[2]]
+        #self.set_pos(position_below_ball)
+        self.close_gripper()
+        self.set_pos_name('home')
+    def place_ball(self, position):
+        self.set_pos(position)
+        self.open_gripper()
+        self.set_pos_name('home')
+        
 
 class MovementManager:
 
-	def __init__(self, cManager):
-	
-		self.cManager = cManager
-		
-	def move_to_target(self, ball):
-		# Create goal:
-		goal = MoveBaseGoal()
-
-		# Set random goal:
-		goal.target_pose.header.frame_id = "map"
-		goal.target_pose.header.stamp = rospy.Time.now()
-		goal.target_pose.pose.position.x = ball.position.position.x
-		goal.target_pose.pose.position.y = ball.position.position.y
-		goal.target_pose.pose.orientation.w = 1.0
-		
-		self.cManager.send_goal_move_base(goal)
-
-class ConnectionManager:
-	
 	def __init__(self):
-	
-		self.move_base = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+		
+		self.move_base = actionlib.SimpleActionClient('move_base', 	MoveBaseAction)
 		self.move_base.wait_for_server(rospy.Duration(5.0))
+		self.finished = False
 		rospy.logdebug("move_base is ready")
 		
-	def send_goal_move_base(self, goal):
+		self.odom_sub = rospy.Subscriber('/odom', Odometry, self.callback)
+		self.robot_pos = Pose()
+		self.arm_controller = ArmController()
+	def callback(self, msg):
+		self.robot_pos = msg.pose.pose
+	def move_to_target(self, pos):
+		# Create goal:
+		goal = MoveBaseGoal()
+		
+		goal.target_pose.header.frame_id = "map"
+		goal.target_pose.header.stamp = rospy.Time.now()
+		goal.target_pose.pose = pos
 		
 		self.move_base.send_goal(goal, self.goal_status)
 		
-	
+	def pickup_routine(self, pose):
+		pose.position.x +=0.2
+		pose.orientation.z=1
+		self.move_to_target(pose)
+		while not self.finished:
+			print('still moving!')
+		self.finished =False
+		self.arm_controller.pick_ball([0.23, 0, 0.029])
+	def delivery_routine(self, pose):
+		pose.position.x = 0.0
+		pose.orientation.z=0
+		pose.orientation.w=1
+		self.move_to_target(pose)
+		self.finished =False
+		while not self.finished:
+			print('still moving!')
+		self.finished =False
+		rospy.sleep(1)
+		self.arm_controller.place_ball([0.2, 0, 0.0275])
 	def goal_status(self, status, result):
 		""" Check the status of a goal - goal reached, aborted,
 		or rejected.
 		"""
-		
-		# Goal reached
-		if status == 3:
-			
-			rospy.loginfo("Goal succeeded")
+		self.finished = True
+	def create_obstacle_msg(self,x, y, radius):
+		obstacle = ObstacleMsg()
+		obstacle.polygon.points = [Point32(x=x+radius, y=y)]
+		angle_step = 2 * math.pi / 16
+		for i in range(16):
+			angle = i * angle_step
+			obstacle.polygon.points.append(Point32(x=x+radius*math.cos(angle), 				y=y+radius*math.sin(angle)))
+		return obstacle
+	def set_obstacle(self, balls):
 
-		# Goal aborted
-		if status == 4:
-			rospy.loginfo("Goal aborted")
+		pub = rospy.Publisher('/obstacle_array', ObstacleArrayMsg, queue_size=10)
+		obstacles = ObstacleArrayMsg()
+		for ball in balls:
+			obstacle = self.create_obstacle_msg(ball[0],ball[1],ball[2])
+			obstacles.obstacles.append(obstacle)
+		rospy.sleep(1)
+		pub.publish(obstacles)
 
-		# Goal rejected
-		if status == 5:
-			rospy.loginfo("Goal rejected")
-            
+
 def main():
 
 	rospy.init_node('movement_manager_node')
 	
-	cManager = ConnectionManager()
-	mManager = MovementManager(cManager)
+	mManager = MovementManager()
 	
 
-	ball = Ball()
-	ball.position.position.x = -1.12
-	ball.position.position.y = 0
+	ball2 = Ball(position= Pose(position= Point(x=-1.5,y=0.7,z=0)))
+	ball1 = Ball(position= Pose(position= Point(x=-1.5,y=-0.7,z=0)))
+	ball0 = Ball(position= Pose(position= Point(x=-1.5,y=0,z=0)))
+	balls = [ball0,ball1,ball2]
+	balls_obs = [[-1.5, 0, 0.0275], [-1.5, -0.7, 0.0275], [-1.5, 0.7, 0.0275]]
+	'''
+	while not mManager.robot_pos:
+		print('didnot recieve position')
 	
-	mManager.move_to_target(ball)
+	result = mManager.orient_robot(ball.position)
+	
+	print(result)
+	
+	rospy.spin()
+	'''
+	mManager.set_obstacle(balls_obs)
+	for ball in balls:
+		mManager.pickup_routine(ball.position)
+		rospy.sleep(1)
+		mManager.delivery_routine(ball.position)
 	
 
 if __name__=='__main__':
--- robotics_challenge/nodes/test.py
+++ robotics_challenge/nodes/test.py
@@ -1,25 +1,75 @@
 #!/usr/bin/env python3
 
 import rospy
-import cv2
+import math
 import numpy as np
+import tf2_ros
+from geometry_msgs.msg import Point, PointStamped
+from opencv_apps.msg import CircleArrayStamped
 from image_geometry import PinholeCameraModel
 from sensor_msgs.msg import CameraInfo
 
+BALL_SIZE = 0.0275 # meters (example size)
 camera = PinholeCameraModel()
-def callback(info):
+pub = rospy.Publisher('/balls_location',Point)
+
+def circle_callback(array):
+	info = rospy.wait_for_message('/camera/rgb/camera_info', CameraInfo)
 	camera.fromCameraInfo(info)
-	
-	pixel = (181,325)
-	print(pixel)
-	point = camera.projectPixelTo3dRay(pixel)
-	print(point)
+	fx = info.K[0]
+	circles = array.circles
+	points = []
+	for circle in circles:
+		print(circle)
+		ball_size_pixels = circle.radius # pixels (example size)
+		distance = BALL_SIZE * fx / ball_size_pixels
+		point = camera.projectPixelTo3dRay((circle.center.x,circle.center.y))
+		print('before converstion')
+		print(point)
+		point = tuple(distance * axis for axis in point)
+		print('before transform')
+		print(point)
+		dpoint = Point(x=point[0],y=point[2],z=point[1])
+    # Transform the point from camera to base frame
+		tf_buffer = tf2_ros.Buffer()
+		tf_listener = tf2_ros.TransformListener(tf_buffer)
+		transform = tf_buffer.lookup_transform('base_link', info.header.frame_id, rospy.Time())
+		translation = np.array([transform.transform.translation.x,
+								transform.transform.translation.y,
+								transform.transform.translation.z])
+		rotation = np.array([transform.transform.rotation.x,
+							transform.transform.rotation.y,
+							transform.transform.rotation.z,
+							transform.transform.rotation.w])
+		R = quaternion_matrix(rotation)[:3,:3]
+		point_base = np.dot(R, point) + translation
+	# Create a PointStamped message for the 3D world point
+		world_point = PointStamped()
+		world_point.header.frame_id = 'world'
+		world_point.header.stamp = rospy.Time.now()
+		world_point.point.x = point_base[0]
+		world_point.point.y = point_base[1]
+		world_point.point.z = point_base[2]
+		print(world_point)
+
+def quaternion_matrix(quaternion):
+    q = np.array(quaternion[:4], dtype=np.float64, copy=True)
+    n = np.dot(q, q)
+    if n < 0.0001:
+        return np.identity(4)
+    q *= np.sqrt(2.0 / n)
+    q = np.outer(q, q)
+    return np.array([
+        [1.0-q[2, 2]-q[3, 3], q[1, 2]-q[3, 0], q[1, 3]+q[2, 0], 0.0],
+        [q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3], q[2, 3]-q[1, 0], 0.0],
+        [q[1, 3]-q[2, 0], q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
+        [0.0, 0.0, 0.0, 1.0]])
+
 
 def main():
 	rospy.init_node('test_node')
-	
 
-	sub = rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, callback)
+	sub_ball = rospy.Subscriber('/hough_circles/circles',CircleArrayStamped, circle_callback)
 	rospy.spin()
 
 if __name__=='__main__':
